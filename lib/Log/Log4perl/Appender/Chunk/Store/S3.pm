36f5cf96db6fae02e49f3706f4201afe9b9e0fac
Jerome Eteve <jerome@broadbean.com>
2014-04-10 16:19:34 +0100

    Basic test for S3 store

--- lib/Log/Log4perl/Appender/Chunk/Store/S3.pm
+++ lib/Log/Log4perl/Appender/Chunk/Store/S3.pm
@@ -6,13 +6,24 @@ extends qw/Log::Log4perl::Appender::Chunk::Store/;
 use Net::Amazon::S3;
 use Net::Amazon::S3::Client;
 
+use Log::Log4perl;
+my $LOGGER = Log::Log4perl->get_logger();
+
+sub BEGIN{
+    eval "require Net::Amazon::S3::Client;";
+    if( $@ ){
+        die "\n\nFor ".__PACKAGE__.": Cannot load Net::Amazon::S3::Client\n\n -> Please install that if you want to use S3 Log Chunk storage.\n\n";
+    }
+}
+
 has 's3_client' => ( is => 'ro', isa => 'Net::Amazon::S3::Client', lazy_build => 1 );
+has 'bucket' => ( is => 'ro' , isa => 'Net::Amazon::S3::Client::Bucket', lazy_build => 1);
+
 
 has 'bucket_name' => ( is => 'ro' , isa => 'Str' , required => 1);
 has 'aws_access_key_id' => ( is => 'ro' , isa => 'Str', required => 1 );
 has 'aws_secret_access_key' => ( is => 'ro' , isa => 'Str' , required => 1);
-
-has 's3config' => ( is => 'ro' , isa => 'HashRef', default => sub{ {}; });
+has 'retry' => ( is => 'ro' , isa => 'Bool' , required => 1 , default => 1);
 
 has 'vivify_bucket' => ( is => 'ro' , isa => 'Bool' , required => 1 , default => 0 );
 
@@ -22,7 +33,7 @@ sub _build_s3_client{
                                          Net::Amazon::S3->new(
                                                               aws_access_key_id => $self->aws_access_key_id(),
                                                               aws_secret_access_key => $self->aws_secret_access_key(),
-                                                              %{$self->s3config()}
+                                                              retry => $self->retry()
                                                              ));
 }
 
@@ -41,33 +52,31 @@ sub clone{
     return __PACKAGE__->new({bucket_name => $self->bucket_name(),
                              aws_access_key_id => $self->aws_access_key_id(),
                              aws_secret_access_key => $self->aws_secret_access_key(),
+                             retry => $self->retry(),
                              vivify_bucket => $self->vivify_bucket()
                             });
 }
 
-=head2 bucket
-
-Returns a the L<Net::Amazon::S3::Client::Bucket>. Autovivify it
-if the vivify_bucket is set to 1. Dies if something is wrong.
-
-Usage:
-
-  my $bucket = $this->bucket();
-
-=cut
-
-sub bucket{
+sub _build_bucket{
     my ($self) = @_;
 
+    my $s3_client = $self->s3_client();
     my $bucket_name = $self->bucket_name();
-    my $bucket = $self->s3_client()->bucket( name => $bucket_name );
-    unless( $bucket ){
-        if( $self->vivify_bucket() ){
-            return $self->s3_client()->create_bucket( name => $bucket_name );
+
+    # Try to hit an existing bucket from the list
+    my @buckets = $s3_client->buckets();
+    foreach my $bucket ( @buckets ){
+        if( $bucket->name() eq $bucket_name ){
+            # Hit!
+            return $bucket;
         }
-        confess("No bucket $bucket_name in Amazon S3 account ".$self->aws_access_key_id());
     }
-    return $bucket;
+
+    unless( $self->vivify_bucket() ){
+        confess("Could not find bucket ".$bucket_name." in this account [access_key_id='".$self->aws_access_key_id()."'] and no vivify_bucket option");
+    }
+    $LOGGER->info("Vivifying Amazon S3 bucket '".$bucket_name."' for S3 account [access_key_id='".$self->aws_access_key_id()."']");
+    return $self->s3_client()->create_bucket( name => $bucket_name );
 }
 
 =head2 store
@@ -81,6 +90,7 @@ sub store{
 
     my $s3object = $self->bucket()->object( key => $chunk_id );
     $s3object->put($big_message);
+    return 1;
 }
 
 __PACKAGE__->meta->make_immutable();
--- t/03-store-s3.t
+++ t/03-store-s3.t
@@ -3,6 +3,14 @@ use Test::More;
 
 use Log::Log4perl;
 
+use Data::UUID;
+my $ug =  new Data::UUID;
+
+my $uuid = $ug->create_str();
+
+my $access_key_id = $ENV{AWS_ACCESS_KEY_ID} || 'A-Fake-keyID';
+my $access_key_secret = $ENV{AWS_ACCESS_KEY_SECRET} || 'A-Fake-Secret-Key';
+
 my $conf = q|
 log4perl.rootLogger=TRACE, Chunk
 
@@ -11,9 +19,12 @@ layout_pattern=%m%n
 
 log4perl.appender.Chunk=Log::Log4perl::Appender::Chunk
 log4perl.appender.Chunk.store_class=S3
-log4perl.appender.Chunk.store_args.bucket_name=MyShinyBucket
-log4perl.appender.Chunk.store_args.aws_access_key_id=Boudin
-log4perl.appender.Chunk.store_args.aws_secret_access_key=Blanc
+log4perl.appender.Chunk.store_args.bucket_name=JETEVE-FullMetalBucket-|.$uuid.q|
+log4perl.appender.Chunk.store_args.aws_access_key_id=|.$access_key_id.q|
+log4perl.appender.Chunk.store_args.aws_secret_access_key=|.$access_key_secret.q|
+log4perl.appender.Chunk.store_args.retry=1
+log4perl.appender.Chunk.store_args.vivify_bucket=1
+
 log4perl.appender.Chunk.layout=${layout_class}
 log4perl.appender.Chunk.layout.ConversionPattern=${layout_pattern}
 |;
@@ -23,5 +34,26 @@ Log::Log4perl::init(\$conf);
 ok( my $ca =  Log::Log4perl->appender_by_name('Chunk') , "Ok got Chunk appender");
 ok( my $store = $ca->store() , "Ok got store for the logger");
 ok( $store->s3_client() , "Ok got s3 client");
+
+SKIP:{
+    unless( $ENV{AMAZON_S3_EXPENSIVE_TESTS} ){
+        skip q/No AMAZON_S3_EXPENSIVE_TESTS=1 in the environment. Skipping side effect test.
+
+Also, you will want to set AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY_SECRET AWS_BUCKET in the environment.
+
+Test buckets will be prefixed with JETEVE-FullMetalBucket-
+
+/, 2 ;
+    }
+    ok( my $bucket = $store->bucket() );
+    ok( $store->store('a_key' , 'Some big content'), "Ok can store stuff");
+
+    # Do some cleanup:
+    if( $bucket ){
+        $bucket->object( key => 'a_key' )->delete();
+        $bucket->delete();
+    }
+}
+
 
 done_testing();
