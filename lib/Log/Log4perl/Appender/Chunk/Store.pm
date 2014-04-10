0fe46b4cfc835b07c7e678242b853f90eedf12eb
Jerome Eteve <jerome@broadbean.com>
2014-04-10 18:54:47 +0100

    Started doc

--- lib/Log/Log4perl/Appender/Chunk/Store.pm
+++ lib/Log/Log4perl/Appender/Chunk/Store.pm
@@ -7,3 +7,19 @@ sub store{
 }
 
 __PACKAGE__->meta->make_immutable();
+
+__END__
+
+=head1 NAME
+
+Log::Log4perl::Appender::Chunk::Store - Store adapter baseclass
+
+=head1 DESCRIPTION
+
+This is the baseclass for all Store adapters used by the
+L<Log::Log4perl::Appender::Chunk> appender.
+
+=head1 IMPLEMENTING YOUR OWN
+
+
+=cut
