a9a333fb5a11efc566f6113fb68e654dfdc73c22
Jerome Eteve <jerome@broadbean.com>
2014-04-11 12:04:12 +0100

    Added doc

--- lib/Log/Log4perl/Appender/Chunk/Store.pm
+++ lib/Log/Log4perl/Appender/Chunk/Store.pm
@@ -21,9 +21,42 @@ L<Log::Log4perl::Appender::Chunk> appender.
 
 =head1 IMPLEMENTING YOUR OWN
 
-Make a subclass of this and implement the 'store' method.
+=head2 Write your subclass
+
+Make a Moose subclass of this and implement the 'store' method.
 
 Have a look at the minimalistic code in L<Log::Log4perl::Appender::Chunk::Store::Memory>.
+
+Settings:
+
+Settings should be plain Scalar Moose attributes. They will be injected from the configuration
+file key 'store_args'.
+
+=head2 Use your Store from the config file.
+
+Set the store_class property of your Chunk appender to something like:
+
+  log4perl.appender.Chunk.store_class=+My::L4p::Appender::Chunk::Store::MyStorage
+
+Remember you can set some your storage class parameters like:
+
+  log4perl.appender.Chunk.store_args.my_setting1=Setting1Value
+  log4perl.appender.Chunk.store_args.my_setting2=Setting2Value
+
+
+=head2 Use your Store from your application code.
+
+If your Storage is too complex to build itself only from the configuration file properties, you can perfectly
+build an instance of it and inject it in your Chunk Appender at run time (do that only once right after L4P init):
+
+  my $store = .. An instance of your My::L4p::Appender::Chunk::Store::MyStorage
+  if( my $chunk_appender = Log::Log4perl->appender_by_name('Chunk') ){
+    $chunk_appender->store($store);
+  }
+
+Don't forget to change 'Chunk' by whatever name you gave to your Chunk appender in the config file.
+
+=head1 METHODS
 
 =head2 store
 
